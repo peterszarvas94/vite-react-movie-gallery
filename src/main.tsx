430f12bb80ba0ce739d68ec554e6a26ab47d582c
Szarvas Péter <peterszarvas94@gmail.com>
2023-06-09 09:47:46 +0200

    fix: another import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import AppContextProvider from './components/AppContext.tsx'
+import AppContextProvider from './utils/AppContext.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
